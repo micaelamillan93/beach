b16f1dec7ab99ecfef7823a81343358418a7cd99
Micaela Millan <mmillan014@gmail.com>
2016-08-04 09:43:56 -0400

    A message will find this later.

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(480, 480);
+  //sky
+  background(51, 192, 214);
+  //sand
+  fill(227, 217, 79);
+  noStroke();
+  rect(0, 250, 480, 240);
+  //sun
+  fill(240, 252, 0);
+  ellipse(0, 0, 150, 150);
+  //umbrella 1
+  fill(237, 17, 17);
+  ellipse(160, 150, 250, 160);
+  //umbrella 1 block
+  fill(51, 192, 214);
+  rect(0, 150, 300, 100);
+   //umbrella post 1
+  fill(255);
+  noStroke();
+  rect(150, 150, 20, 200);
+  //ball
+  fill(17, 237, 46);
+  ellipse (300, 275, 100, 100);
+  
+}
+
+function draw() {
+  
+}
